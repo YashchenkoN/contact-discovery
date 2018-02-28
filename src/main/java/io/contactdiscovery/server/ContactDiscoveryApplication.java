aea17479204a0b25f55be464e8c0e39b8c98635f
kolyan <vkont4@gmail.com>
2018-02-28 20:40:38 +0200

    Application init

---
+++ src/main/java/io/contactdiscovery/server/ContactDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package io.contactdiscovery.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactDiscoveryApplication.class, args);
+	}
+}
