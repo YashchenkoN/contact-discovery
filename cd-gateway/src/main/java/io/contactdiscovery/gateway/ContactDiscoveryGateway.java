3838d41e775e2b960d07fa46728914244d89baa9
kolyan <vkont4@gmail.com>
2018-04-12 17:32:05 +0300

    All properties moved to config server

---
+++ cd-gateway/src/main/java/io/contactdiscovery/gateway/ContactDiscoveryGateway.java
@@ -0,0 +1,19 @@
+package io.contactdiscovery.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Mykola Yashchenko
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ContactDiscoveryGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactDiscoveryGateway.class, args);
+    }
+}
