0cf6b5d388135a3e3092b9c456e7ea609829df33
kolyan <vkont4@gmail.com>
2018-04-13 13:29:24 +0300

    Tests bootstrap for CD-Service

---
+++ cd-service/src/test/java/io/contactdiscovery/service/ContactDiscoveryServiceTest.java
@@ -0,0 +1,19 @@
+package io.contactdiscovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * @author Mykola Yashchenko
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = ContactDiscoveryServiceApplication.class)
+public class ContactDiscoveryServiceTest {
+
+    @Test
+    public void shouldLoadContext() {
+
+    }
+}
