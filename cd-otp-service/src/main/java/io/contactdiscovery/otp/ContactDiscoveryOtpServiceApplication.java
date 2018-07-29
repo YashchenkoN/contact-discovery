131e259e5075f68eff40fc8c57918f81a596c5a5
kolyan <vkont4@gmail.com>
2018-07-29 13:34:25 +0300

    Added OTP service

---
+++ cd-otp-service/src/main/java/io/contactdiscovery/otp/ContactDiscoveryOtpServiceApplication.java
@@ -0,0 +1,12 @@
+package io.contactdiscovery.otp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactDiscoveryOtpServiceApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ContactDiscoveryOtpServiceApplication.class, args);
+    }
+}
---
+++ cd-otp-service/src/test/java/io/contactdiscovery/otp/ContactDiscoveryOtpServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package io.contactdiscovery.otp;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = ContactDiscoveryOtpServiceApplication.class)
+public class ContactDiscoveryOtpServiceApplicationTest {
+
+    @Test
+    public void shouldLoadContext() {
+
+    }
+}
