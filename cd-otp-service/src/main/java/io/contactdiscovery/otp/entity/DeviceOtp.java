5447a6539c900eb966b6ea09cffa056278c441ce
kolyan <vkont4@gmail.com>
2018-07-31 22:04:03 +0300

    Added new API for device registration

---
+++ cd-otp-service/src/main/java/io/contactdiscovery/otp/entity/DeviceOtp.java
@@ -0,0 +1,9 @@
+package io.contactdiscovery.otp.entity;
+
+/**
+ * @author Mykola Yashchenko
+ */
+public class DeviceOtp {
+    private String deviceId;
+    private String seed;
+}
---
+++ cd-service/src/main/java/io/contactdiscovery/service/api/RegisterDeviceRequest.java
@@ -0,0 +1,8 @@
+package io.contactdiscovery.service.api;
+
+/**
+ * @author Mykola Yashchenko
+ */
+public class RegisterDeviceRequest {
+    private String phoneNumber;
+}
