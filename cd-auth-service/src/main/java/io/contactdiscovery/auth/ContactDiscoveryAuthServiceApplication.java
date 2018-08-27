5f841836c318694b0d1937318c4054d388a90b7a
kolyan <vkont4@gmail.com>
2018-08-27 10:23:32 +0300

    Added Auth Service App

---
+++ cd-auth-service/src/main/java/io/contactdiscovery/auth/ContactDiscoveryAuthServiceApplication.java
@@ -0,0 +1,15 @@
+package io.contactdiscovery.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mykola Yashchenko
+ */
+@SpringBootApplication
+public class ContactDiscoveryAuthServiceApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ContactDiscoveryAuthServiceApplication.class, args);
+    }
+}
