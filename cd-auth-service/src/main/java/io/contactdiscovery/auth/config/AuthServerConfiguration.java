d418663a9f02445040cf0c74714eb75c0548fd85
kolyan <vkont4@gmail.com>
2018-08-29 14:00:00 +0200

    Auth server webflux

--- cd-auth-service/src/main/java/io/contactdiscovery/auth/config/AuthServerConfiguration.java
+++ cd-auth-service/src/main/java/io/contactdiscovery/auth/config/AuthServerConfiguration.java
@@ -1,7 +1,13 @@
 package io.contactdiscovery.auth.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
 /**
  * @author Mykola Yashchenko
  */
-public class AuthServerConfiguration {
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfiguration extends AuthorizationServerConfigurerAdapter {
 }
