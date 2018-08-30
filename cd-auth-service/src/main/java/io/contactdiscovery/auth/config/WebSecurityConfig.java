eb202c4472850dd1cf76b05b97ce54216e916b39
kolyan <vkont4@gmail.com>
2018-08-30 14:00:00 +0200

    Auth web security config

--- cd-auth-service/src/main/java/io/contactdiscovery/auth/config/WebSecurityConfig.java
+++ cd-auth-service/src/main/java/io/contactdiscovery/auth/config/WebSecurityConfig.java
@@ -1,7 +1,31 @@
 package io.contactdiscovery.auth.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
  * @author Mykola Yashchenko
  */
-public class WebSecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }
