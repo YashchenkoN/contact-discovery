545dd517bf8f7eabfddc358326b0aab4068099bd
kolyan <vkont4@gmail.com>
2018-09-01 14:00:00 +0200

    Auth web security config

--- cd-auth-service/src/main/java/io/contactdiscovery/auth/config/AuthServerConfiguration.java
+++ cd-auth-service/src/main/java/io/contactdiscovery/auth/config/AuthServerConfiguration.java
@@ -1,6 +1,7 @@
 package io.contactdiscovery.auth.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
@@ -10,4 +11,27 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    @Override
+    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+        // @formatter:off
+        clients
+                .inMemory()
+                .withClient("browser")
+                .secret("{noop}secret")
+                .authorizedGrantTypes("refresh_token", "password")
+                .scopes("ui")
+                .and()
+                .withClient("cd-query")
+                .secret("{noop}123")
+                .authorizedGrantTypes("client_credentials", "refresh_token")
+                .scopes("server")
+                .and()
+                .withClient("cd-service")
+                .secret("{noop}123")
+                .authorizedGrantTypes("client_credentials", "refresh_token")
+                .scopes("server");
+        // @formatter:on
+    }
+
 }
