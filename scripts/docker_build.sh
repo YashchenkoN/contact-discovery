9df5444723aa26db31edebbbc1f7cb40f5f8ef10
kolyan <vkont4@gmail.com>
2018-09-02 14:00:00 +0200

    Return exit code

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -4,12 +4,12 @@ mvn clean install
 
 if [[ "$?" -ne 0 ]] ; then
     echo 'Maven build failed';
-    exit
+    exit 1
 fi
 
 mvn docker:build -pl cd-config,cd-discovery,cd-gateway,cd-query,cd-service,cd-otp-service,cd-auth-service
 
 if [[ "$?" -ne 0 ]] ; then
     echo 'Maven docker build failed';
-    exit
+    exit 1
 fi
